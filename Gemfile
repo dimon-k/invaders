40250e47ab009b48d19518a5c7341f1edbbad29a
Dmitriy Kanunnikov <dmitriykanunnikov@ip-192-168-1-92.eu-west-1.compute.internal>
2021-06-17 23:49:20 +0200

    Initiate project

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'pry', '~> 0.13.1'
+gem 'rake', '~> 13.0'
+gem 'rspec', '~> 3.0'
+gem 'rubocop', '~> 1.7'
---
+++ lib/invaders.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Dir['./lib/invaders/*.rb'].sort.each { |file| require file }
+
+module Invaders
+  # This is the main App class
+  class App
+  end
+end
