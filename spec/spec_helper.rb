49cc7b9087829a3d17c37df7ff1b65d4a6197ea2
Dmitriy Kanunnikov <dmitriykanunnikov@ip-192-168-1-92.eu-west-1.compute.internal>
2021-06-18 13:21:34 +0200

    Config rspec_status

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require 'invaders'
 require 'pry'
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = '.rspec_status'
-
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
