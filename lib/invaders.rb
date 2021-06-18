581f2ca4cabbffb30ef846792a6b3e2f493fd4e8
Dmitriy Kanunnikov <dmitriykanunnikov@ip-192-168-1-92.eu-west-1.compute.internal>
2021-06-18 13:20:27 +0200

    Adjust 'Invaders::App' class

--- lib/invaders.rb
+++ lib/invaders.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 Dir['./lib/invaders/*.rb'].sort.each { |file| require file }
 
 module Invaders
   # This is the main App class
   class App
+    # If 'true', will print on the radar example possible locations
+    # with 'X' sign indicating top left corner of detected invaders
+    PRINT_LOCATION_ON_RADAR = true
+
+    def call
+      Invaders::Catalog.new.call.each.with_index(1) do |invader, invader_index|
+        puts "\nInvader #{invader_index}:".colorize(:green)
+        puts invader.map(&:join).join("\n").colorize(:red)
+
+        Invaders::Scanner.new(
+          invader: invader,
+          radar_result: Invaders::Radar.new.call
+        ).call
+      end
+    end
   end
+
+  Invaders::App.new.call
 end
