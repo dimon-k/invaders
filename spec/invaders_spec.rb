9125922a537386460f4f309b8e420427e2500aa7
Dmitriy Kanunnikov <dmitriykanunnikov@ip-192-168-1-92.eu-west-1.compute.internal>
2021-06-18 13:21:11 +0200

    Add invaders app specs

--- spec/invaders_spec.rb
+++ spec/invaders_spec.rb
@@ -7,7 +7,36 @@ RSpec.describe Invaders do
     expect(Invaders::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe Invaders::App do
+    subject(:invaders_app) { described_class.new.call }
+
+    let(:invaders_scanner) { instance_double(Invaders::Scanner, call: true) }
+    let(:invader_1_message) { "\e[0;32;49m\nInvader 1:\e[0m" }
+    let(:invader_2_message) { "\e[0;32;49m\nInvader 2:\e[0m" }
+    let(:invader_1_look) do
+      "\e[0;31;49m--o-----o--\n---o---o---\n--ooooooo--\n-oo-ooo-oo-\nooooooooooo\no-ooooooo-o\no-o-----o-o\n---oo-oo---\e[0m"
+    end
+    let(:invader_2_look) do
+      "\e[0;31;49m---oo---\n--oooo--\n-oooooo-\noo-oo-oo\noooooooo\n--o--o--\n-o-oo-o-\no-o--o-o\e[0m"
+    end
+
+    before do
+      allow(Invaders::Catalog).to receive(:new).and_call_original
+      allow(Invaders::Scanner).to receive(:new).and_return(invaders_scanner)
+    end
+
+    it 'puts invaders', :aggregate_failures do
+      expect($stdout).to receive(:puts).with(invader_1_message)
+      expect($stdout).to receive(:puts).with(invader_1_look)
+      expect($stdout).to receive(:puts).with(invader_2_message)
+      expect($stdout).to receive(:puts).with(invader_2_look)
+      invaders_app
+    end
+
+    it 'runns the app', :aggregate_failures do
+      expect(Invaders::Catalog).to receive(:new).once
+      expect(invaders_scanner).to receive(:call).twice
+      invaders_app
+    end
   end
 end
